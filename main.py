bee5555308f1e76cce82fbc59ef054961288f0c4
Mikayla Beelders <mikaylabeelders@gmail.com>
2021-06-08 10:12:51 +0200

    Adding mini-calculator file

---
+++ main.py
@@ -0,0 +1,14 @@
+class Calculator():
+
+    def add (self, a, b):
+        return a + b
+
+    def subtract (self, a, b):
+        return a - b
+
+    def multipy (self, a, b):
+        return a * b
+
+    def divide (self, a, b):
+        return a / b
+
